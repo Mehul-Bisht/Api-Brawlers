873cd0618af882f1c71872ace1ffcd86acc1a6b7
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-11-07 14:03:42 +0530

    link changes

--- src/BrawlerData.kt
+++ src/BrawlerData.kt
@@ -7,27 +7,33 @@ object BrawlerData {
 
     val leon = Brawler("Leon",
             "Legendary",
-            "https://drive.google.com/file/d/1pRCHaHW0JxXxbn7Aa6MFPJRlreZ0ADMd/view?usp=sharing")
+            //"https://drive.google.com/file/d/1pRCHaHW0JxXxbn7Aa6MFPJRlreZ0ADMd/view?usp=sharing"
+               "https://drive.google.com/uc?export=view&id=1pRCHaHW0JxXxbn7Aa6MFPJRlreZ0ADMd")
 
     val sandy = Brawler("Sandy",
             "Legendary",
-            "https://drive.google.com/file/d/1-bWZbF8SAOj6xBIXPB8t-sNurBitZhTW/view?usp=sharing")
+            //"https://drive.google.com/file/d/1-bWZbF8SAOj6xBIXPB8t-sNurBitZhTW/view?usp=sharing"
+               "https://drive.google.com/uc?export=view&id=1-bWZbF8SAOj6xBIXPB8t-sNurBitZhTW")
 
     val spike = Brawler("Spike",
             "Legendary",
-            "https://drive.google.com/file/d/102xNAwjiqZzDVhbw2kics-2bk7Seml7_/view?usp=sharing")
+            //"https://drive.google.com/file/d/102xNAwjiqZzDVhbw2kics-2bk7Seml7_/view?usp=sharing"
+               "https://drive.google.com/uc?export=view&id=102xNAwjiqZzDVhbw2kics-2bk7Seml7_")
 
     val mortis = Brawler("Mortis",
             "Mythic",
-            "https://drive.google.com/file/d/1LlYRbH5fJVhcZl-rg8-un5OT0Y5-B0GH/view?usp=sharing")
+            //"https://drive.google.com/file/d/1LlYRbH5fJVhcZl-rg8-un5OT0Y5-B0GH/view?usp=sharing"
+              "https://drive.google.com/uc?export=view&id=1LlYRbH5fJVhcZl-rg8-un5OT0Y5-B0GH")
 
     val max = Brawler("Max",
             "Mythic",
-            "https://drive.google.com/file/d/1_7tR1JmIOIee55Ot11HSq4Wz-PYqQNd9/view?usp=sharing")
+            //"https://drive.google.com/file/d/1_7tR1JmIOIee55Ot11HSq4Wz-PYqQNd9/view?usp=sharing"
+               "https://drive.google.com/uc?export=view&id=1_7tR1JmIOIee55Ot11HSq4Wz-PYqQNd9")
 
     val jessie = Brawler("Jessie",
             "Trophy road",
-            "https://drive.google.com/file/d/1PdWfJ-ovmE_BHxwHWoXgsoba4xZJQF9X/view?usp=sharing")
+            //"https://drive.google.com/file/d/1PdWfJ-ovmE_BHxwHWoXgsoba4xZJQF9X/view?usp=sharing"
+               "https://drive.google.com/uc?export=view&id=1PdWfJ-ovmE_BHxwHWoXgsoba4xZJQF9X")
 
     val brawlers = listOf(amber, leon, sandy, spike, mortis, max, jessie)
 
